9638bcc8d77117673d0124fd7b229ea7cc90eb9a
Pragyan Aryal <pragyanaryal4@gmail.com>
2025-02-11 01:52:20 -0600

    Setup http server command starter

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"kai_security/internal/actor"
+	"kai_security/internal/config"
+	"kai_security/internal/logger"
+)
+
+func main() {
+	_ = config.Get()
+	_ = logger.Get()
+
+	server := actor.NewHttpServer()
+
+	server.Start()
+}
